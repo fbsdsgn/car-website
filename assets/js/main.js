f72ed6a3d6be54d9e2f0c488a2c59c03e9e42ac5
Felipe B. Souza <fbsdsgn@gmail.com>
2022-08-26 11:42:02 -0300

    Adding click event to show and hide scroll up component

--- assets/js/main.js
+++ assets/js/main.js
@@ -75,3 +75,12 @@ function activeFeatured() {
 }
 
 linkFeatured.forEach((l) => l.addEventListener("click", activeFeatured));
+
+/*======= SHOW SCROLL UP ======= */
+function scrollUp() {
+  const scrollUp = document.getElementById("scroll-up");
+  if (this.scrollY >= 350) scrollUp.classList.add("show-scroll");
+  else scrollUp.classList.remove("show-scroll");
+}
+
+window.addEventListener("scroll", scrollUp);
